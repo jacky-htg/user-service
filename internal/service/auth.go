d53507c9a78b7bd9806adfdb4d2bfb1fdf4b31c4
Rijal Asepnugroho <rasepnugroho@waresix.com>
2021-01-08 19:58:02 +0700

    add get metadata function

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -3,6 +3,12 @@ package service
 import (
 	"context"
 	"database/sql"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+
+	"user-service/internal/pkg/app"
 	"user-service/internal/pkg/db/redis"
 	users "user-service/pb"
 )
@@ -36,4 +42,43 @@ func (u *Auth) ChangePassword(context.Context, *users.ChangePasswordRequest) (*u
 // IsAuth service
 func (u *Auth) IsAuth(context.Context, *users.Id) (*users.Boolean, error) {
 	return &users.Boolean{}, nil
+}
+
+func getMetadataToken(ctx context.Context) (context.Context, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return ctx, status.Errorf(codes.Unauthenticated, "metadata is not provided")
+	}
+
+	token := md["token"]
+	if len(token) == 0 {
+		return ctx, status.Errorf(codes.Unauthenticated, "authorization token is not provided")
+	}
+
+	ctx = context.WithValue(ctx, app.Ctx("token"), token[0])
+
+	return ctx, nil
+}
+
+func getMetadata(ctx context.Context) (context.Context, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return ctx, status.Errorf(codes.Unauthenticated, "metadata is not provided")
+	}
+
+	userID := md["user_id"]
+	if len(userID) == 0 {
+		return ctx, status.Errorf(codes.Unauthenticated, "user_id is not provided")
+	}
+
+	ctx = context.WithValue(ctx, app.Ctx("userID"), userID[0])
+
+	companyID := md["company_id"]
+	if len(companyID) == 0 {
+		return ctx, status.Errorf(codes.Unauthenticated, "company_id is not provided")
+	}
+
+	ctx = context.WithValue(ctx, app.Ctx("companyID"), companyID[0])
+
+	return ctx, nil
 }
