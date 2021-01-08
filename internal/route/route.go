24417dc3dcf68a4c40a19faea7d7b9f0f6193d26
Rijal Asepnugroho <rasepnugroho@waresix.com>
2021-01-08 18:38:36 +0700

    add auth routing

--- internal/route/route.go
+++ internal/route/route.go
@@ -7,9 +7,12 @@ import (
 	"google.golang.org/grpc"
 
 	"user-service/internal/pkg/db/redis"
+	"user-service/internal/service"
+	users "user-service/pb"
 )
 
 // GrpcRoute func
 func GrpcRoute(grpcServer *grpc.Server, db *sql.DB, log *logrus.Entry, cache *redis.Cache) {
-
+	authServer := service.Auth{Db: db, Cache: cache}
+	users.RegisterAuthServiceServer(grpcServer, &authServer)
 }
